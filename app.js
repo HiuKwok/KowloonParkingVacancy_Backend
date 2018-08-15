9db4499aff1335448c13dff5484f1b6e7fafe330
Hiu Kwok <hiufkwok@gmail.com>
2018-08-15 16:38:34 +0800

    Enhencement: Enable DB pooling for /carparks/:id endpoint

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ let limitCount = 10;
 
 const connectionString = config.connectionString;
 
+//Establish DB pool
+const pool = new pg.Pool({
+    connectionString : config.connectionString,
+})
+
+
+
 app.get('/carparks', function (req, res) {
 
     //Only resolve request when limitCount >0
@@ -43,19 +50,14 @@ app.get('/carparks', function (req, res) {
 });
 
 
-
 app.get('/carparks/:id', function (req, res) {
 
-    const client = new pg.Client(connectionString);
-    client.connect();
-    //TBC: Need to validate data first
-    up.getVacancyInfoByID(client, req.params.id)
+    up.getVacancyInfoByID(pool, req.params.id)
         .then( (data) => {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(data) );
         })
-        .catch(util.onRejectPrintMsg)
-        .then(() => { client.end();});
+        .catch(util.onRejectPrintMsg);
 });
 
 
@@ -129,4 +131,16 @@ app.listen(3000, function (req, res) {
     //Set header to JSON format(REST API)
     //res.setHeader('Content-Type', 'application/json');
     console.log('Example app listening on port 3000!');
+});
+
+
+process.on('SIGINT', function () {
+    pool.end().then(() => {
+        console.log('pool has ended');
+        process.exit();
+    });
+});
+process.on('exit', function() {
+    console.log("Shutdown Node app!");
+
 });
